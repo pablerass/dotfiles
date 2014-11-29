cb81623d3f826e319533af51740dfc21a962cfd4
Pablo Muñoz <pablerass@gmail.com>
2014-11-29 13:37:54 +0100

    Upgraded based on iperdrazas/dotfiles

---
+++ setup/install-shell.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Update repos and packages
+sudo apt-get update -y
+
+# Install packages
+sudo apt-get install git vim zsh htop curl -y
+
+# Install oh-my-zsh
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+
+# Remove current config files
+cd ~
+
+if [ -f .gitconfig ];
+then
+	mv .gitconfig .gitconfig.bak
+fi
+
+if [ -f .zshrc ];
+then
+	mv .zshrc .zshrc.bak
+fi
+
+if [ -f .vimrc ];
+then
+	mv .vimrc .vimrc.bak
+fi
+
+# Set new config files
+ln -s dotfiles/bin bin
+ln -s dotfiles/gitconfig .gitconfig
+ln -s dotfiles/vimrc .vimrc
+ln -s dotfiles/zshrc .zshrc
+
+# Configure vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/swaps
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/undos
+
+# Set default shell
+sudo chsh -s $(which zsh) $USER
---
+++ zshrc
@@ -0,0 +1,18 @@
+# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+ZSH_THEME="candy"
+
+plugins=(git vagrant ssh-agent docker systemadmin pip python pyenv)
+
+source $ZSH/oh-my-zsh.sh
+
+# Customize
+export PATH=$PATH:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:
+
+source $HOME/dotfiles/alias
+source $HOME/dotfiles/functions
+source $HOME/dotfiles/z.sh
+
+export LANG=es_ES.UTF-8
+
+setxkbmap es
