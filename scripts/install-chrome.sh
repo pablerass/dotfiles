00288b30e343dd1cc6e3762b02db498024066277
Pablo Muñoz <pablerass@gmail.com>
2015-02-23 22:55:29 +0100

    Renamed install scipts folder and chrome installation

---
+++ scripts/install-chrome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Add repositories
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+# Update repos and packages
+sudo apt-get update -y
+
+# Install additional packages
+sudo apt-get install google-chrome-stable -y
