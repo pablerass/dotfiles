8d8e1e7a4add31acdb15c8fc2c4bc5135aac9f01
Pablo Muñoz <pablerass@gmail.com>
2015-03-02 22:32:22 +0100

    Added multiple install scripts and reordered kernel ones

---
+++ scripts/install-kvm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Documentation
+# http://sharadchhetri.com/2014/10/09/install-kvm-kernel-based-virtual-machine-ubuntu-14-04-lts-desktop/
+
+# Add repositories
+
+# Update repos and packages
+sudo apt-get update -y
+
+# Install packages
+sudo apt-get install qemu-kvm libvirt-bin bridge-utils -y
+sudo apt-get install virt-manager -y
+
+# Configure packages
