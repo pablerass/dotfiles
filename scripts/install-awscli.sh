1039b9c4cda3c94d3454f2e66df369807cb8e056
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2015-06-03 15:06:09 +0200

    Added awscli installation and nodejs configuration

---
+++ scripts/install-awscli.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install packages
+#	jq - enables json parsing from bash
+sudo apt-get install jq
+
+# Install python libs
+sudo pip install awscli
--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -7,3 +7,6 @@ sudo apt-get update -y
 
 # Install packages
 sudo apt-get install nodejs npm -y
+
+# Create symbolic link
+sudo ln -s /usr/bin/nodejs /usr/bin/node
