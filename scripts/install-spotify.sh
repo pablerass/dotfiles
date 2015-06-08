9dca6cddb28bcbc5eeaa2ea768e14d35a60537d9
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2015-06-08 08:51:05 +0200

    Corrected Spotify installation script

--- scripts/install-spotify.sh
+++ scripts/install-spotify.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# Uncomment Canonical partner respository
-sed -i'' s/^# $deb http://archive.canonical.com/ubuntu trusty partner
-# deb-src http://archive.canonical.com/ubuntu trusty partner
+# Add repositories
+sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 
-# Download kernel deb packages
+# Update repos and packages
+sudo apt-get update -y
 
-wget http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR1a/eclipse-java-luna-SR1a-linux-gtk-x86_64.tar.gz
-wget kernel.ubuntu.com/~kernel-ppa/mainline/v$dist_ver/linux-headers-$full_ver-generic_$full_ver.${timestamp}_$arch.deb
-wget kernel.ubuntu.com/~kernel-ppa/mainline/v$dist_ver/linux-headers-${full_ver}_$full_ver.${timestamp}_all.deb
-wget kernel.ubuntu.com/~kernel-ppa/mainline/v$dist_ver/linux-image-$full_ver-generic_$full_ver.${timestamp}_amd64.deb
-
-# Install kernel packages in proper order
-sudo dpkg -i linux-headers-$full_ver*.deb linux-image-$full_ver*.deb
-
-# Delete kernel packages
-rm linux-*-$full_ver*.deb
+# Install packages
+sudo apt-get install spotify-client -y
