438261f3177f2c13172027e2937a08d74a250770
Pablo Muñoz <pablerass@gmail.com>
2015-06-14 14:24:32 +0200

    Changed repository management and added Python autocomplete

--- setup/repos.sh
+++ setup/repos.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
+## Variables
 GITHUB_URL=https://github.com
 
+OH_MY_ZSH_PLUGINS_DIR=~/.oh-my-zsh/custom/plugins
+VIM_PLUGINS_DIR=~/.vim/bundle
+
+## Functions
+function update_repo {
+	LOCAL_DIR=$2
+	REPO=$1
+	if [ -d "$LOCAL_DIR" ]
+	then
+		git -C "$LOCAL_DIR" pull origin master
+	else
+		git clone $REPO "$LOCAL_DIR"
+	fi
+}
+
+## Repos
 # oh-my-zsh plugins
-LOCAL_DIR=~/.oh-my-zsh/custom/plugins/virsh
-if [ -d $LOCAL_DIR ]
-then
-	git -C $LOCAL_DIR pull origin master
-else
-	git clone $GITHUB_URL/jplitza/zsh-virsh-autocomplete.git $LOCAL_DIR
-fi
+update_repo $GITHUB_URL/jplitza/zsh-virsh-autocomplete.git $OH_MY_ZSH_PLUGINS_DIR/virsh
 
 # Vim plugins
-LOCAL_DIR=~/.vim/bundle/vim-airline
-if [ -d $LOCAL_DIR ]
-then
-	git -C $LOCAL_DIR pull origin master
-else
-	git clone $GITHUB_URL/bling/vim-airline.git $LOCAL_DIR
-fi
-
-LOCAL_DIR=~/.vim/bundle/vim-fugitive
-if [ -d $LOCAL_DIR ]
-then
-	git -C $LOCAL_DIR pull origin master
-else
-	git clone $GITHUB_URL/tpope/vim-fugitive.git $LOCAL_DIR
-fi
+update_repo $GITHUB_URL/scrooloose/nerdtree.git $VIM_PLUGINS_DIR/nerdtree
+update_repo $GITHUB_URL/bling/vim-airline.git $VIM_PLUGINS_DIR/vim-airline
+update_repo $GITHUB_URL/davidhalter/jedi-vim.git $VIM_PLUGINS_DIR/jedi-vim
+update_repo $GITHUB_URL/tpope/vim-fugitive.git $VIM_PLUGINS_DIR/vim-fugitive
