e0d2e7bb3fcc90d18ebee5dc3ecdb970f00fe182
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2015-07-01 15:02:53 +0200

    Modified mongo and spotify apt repos configuration

--- scripts/install-mongodb.sh
+++ scripts/install-mongodb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Add repositories
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7F0CEB10
 sudo sh -c "echo deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse > /etc/apt/sources.list.d/mongodb.list"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7F0CEB10
 
 # Update repos and packages
 sudo apt-get update -y
--- scripts/install-spotify.sh
+++ scripts/install-spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add repositories
-sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list'
+sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
 
 # Update repos and packages
