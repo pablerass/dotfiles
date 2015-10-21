fa005053730926f82aee302f9d8d612a17489dea
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2015-10-21 11:49:07 +0200

    Create Guake install script

---
+++ scripts/install-guake.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add repositories
+sudo add-apt-repository ppa:caffeine-developers/ppa -y
+
+# Update repos and packages
+sudo apt-get update -y
+
+# Install packages
+sudo apt-get install caffeine -y
+
+# Configure startup indicator
+sed -i 's/\/usr\/bin\/caffeine/\/usr\/bin\/caffeine-indicator/' ~/.config/autostart/caffeine.desktop
