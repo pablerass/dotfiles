26cd0ad2b1e7b1fd831cd72b4610f9749e67603b
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2015-12-07 11:25:37 +0100

    Added ipython installation

--- scripts/configure-python.sh
+++ scripts/configure-python.sh
@@ -4,4 +4,7 @@
 sudo apt-get install python-pip python3-pip -y
 
 # Install python libs
-sudo -H pip3 install --upgrade virtualenv virtualenvwrapper flake8 flake8-docstrings
+sudo -H pip3 install --upgrade \
+		virtualenv virtualenvwrapper \
+		flake8 flake8-docstrings \
+		ipython[all]
