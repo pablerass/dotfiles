67861f051a8b2126f91e3394b985d63434191607
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2016-01-11 17:34:42 +0100

    Add Node Chris Lea PPA removal before installation

--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -5,6 +5,10 @@ source /etc/lsb-release
 VERSION=4.x
 DISTRO=$DISTRIB_CODENAME
 
+# Remove chris-lea-ppa if configured
+sudo add-apt-repository -y -r ppa:chris-lea/node.js
+sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list
+
 # Add repositories
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 sudo sh -c "echo deb https://deb.nodesource.com/node_$VERSION $DISTRO main > /etc/apt/sources.list.d/nodesource.list"
