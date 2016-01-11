0425fe6edebb0e86aed55c12d0f41961c1e28a2a
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2016-01-11 17:31:55 +0100

    Update Node installation script to install newer versions

--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -e
 
+source /etc/lsb-release
+
+VERSION=4.x
+DISTRO=$DISTRIB_CODENAME
+
 # Add repositories
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+sudo sh -c "echo deb https://deb.nodesource.com/node_$VERSION $DISTRO main > /etc/apt/sources.list.d/nodesource.list"
+sudo sh -c "echo deb-src https://deb.nodesource.com/node_$VERSION $DISTRO main >> /etc/apt/sources.list.d/nodesource.list"
 
 # Update repos and packages
 sudo apt-get update -y
 
 # Install packages
-sudo apt-get install nodejs npm -y
+sudo apt-get install nodejs -y
 
 # Create needed symbolic links
-sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo ln -s /etc/bash_completion.d/npm /usr/share/npm/lib/utils/completion.sh
