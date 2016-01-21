f230ee7ebcb7be5562590fe477f3806887f343c4
Pablo Muñoz <pablerass@gmail.com>
2016-01-21 01:14:36 +0100

    Add ecs-cli to awscli installation

--- scripts/install-awscli.sh
+++ scripts/install-awscli.sh
@@ -5,4 +5,9 @@
 sudo apt-get install jq
 
 # Install python libs
-sudo -H pip install awscli
+sudo -H pip install -U awscli
+
+# Install ECS Cli
+sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
+
