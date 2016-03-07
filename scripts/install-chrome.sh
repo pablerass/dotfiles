513a02aaecf2b564aea8bc5d246e8d9942666615
Pablo Muñoz <pablerass@gmail.com>
2016-03-07 09:34:53 +0100

    Correct install chrome repo bugs

--- scripts/install-chrome.sh
+++ scripts/install-chrome.sh
@@ -2,9 +2,7 @@
 
 # Add repositories
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-if [ ! -e /etc/apt/sources.list.d/google-chrome.list ]; then
-	sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-fi
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 
 # Update repos
 sudo apt-get update -y
