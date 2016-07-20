fd3c8f5e9c9b952251c8bace50d5972fa1ca51b7
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2016-07-20 15:42:43 +0200

    Modify scripts and config to work with Ubuntu-16.04

--- scripts/install-caffeine.sh
+++ scripts/install-caffeine.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -e
 
-# Add repositories
-sudo add-apt-repository ppa:caffeine-developers/ppa -y
-
-# Update repos and packages
-sudo apt-get update -y
-
 # Install packages
 sudo apt-get install caffeine -y
 
 # Configure startup indicator
-sed -i 's/\/usr\/bin\/caffeine/\/usr\/bin\/caffeine-indicator/' ~/.config/autostart/caffeine.desktop
+sudo sed -i 's/\/usr\/bin\/caffeine$/\/usr\/bin\/caffeine-indicator/' /etc/xdg/autostart/caffeine.desktop
