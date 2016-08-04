414a97008ec4e59ecbba0b57c00b49a82621f786
Pablo Muñoz <pablo.munoz.orbananos.contractor@bbva.com>
2016-08-04 15:27:51 +0200

    Add ropevim for Python refactoring

--- setup/repos.sh
+++ setup/repos.sh
@@ -32,5 +32,6 @@ update_repo $GITHUB_URL/davidhalter/jedi-vim.git $VIM_PLUGINS_DIR/jedi-vim
 update_repo $GITHUB_URL/tpope/vim-fugitive.git $VIM_PLUGINS_DIR/vim-fugitive
 update_repo $GITHUB_URL/tpope/vim-surround $VIM_PLUGINS_DIR/vim-surround
 update_repo $GITHUB_URL/nvie/vim-flake8.git $VIM_PLUGINS_DIR/vim-flake8
+update_repo $GITHUB_URL/python-rope/ropevim.git $VIM_PLUGINS_DIR/ropevim
 update_repo $GITHUB_URL/JamshedVesuna/vim-markdown-preview \
 			$VIM_PLUGINS_DIR/vim-markdown-preview
