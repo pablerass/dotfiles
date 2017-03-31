17d066cbc0e2c653814674c1ee066f3c436d722a
Pablo Muñoz <pablerass@gmail.com>
2017-03-31 13:56:36 +0200

    Add specific terraform version to terragrunt and update the basic one

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -2,15 +2,14 @@
 
 # Specify version
 app=terraform
-ver=0.8.8
+ver=0.9.2
 arch=amd64
 package=${app}_${ver}_linux_${arch}.zip
 
-# Download deb packages
+# Download packages
 wget -N https://releases.hashicorp.com/$app/$ver/$package
 
 # Install packages
-sudo rm -Rf /opt/$app
 sudo unzip -o -u $package -d /opt/$app
 
 # Delete packages
@@ -18,8 +17,3 @@ rm -f $package
 
 # Configure path
 sudo sh -c "echo 'export PATH=\$PATH:/opt/$app' > /etc/profile.d/${app}.sh"
-
-# Install additional apps
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-$DIR/install-terragrunt.sh
--- scripts/install-terragrunt.sh
+++ scripts/install-terragrunt.sh
@@ -1,18 +1,28 @@
 #!/bin/bash -e
 
 # Specify version
-app=terragrunt
-ver=0.11.0
 arch=amd64
-package=${app}_linux_${arch}
 
-# Download deb packages
-wget -N https://github.com/gruntwork-io/$app/releases/download/v$ver/$package
+grunt_app=terragrunt
+grunt_ver=0.11.0
+grunt_package=${grunt_app}_linux_${arch}
+
+form_app=terraform
+form_ver=0.8.8
+form_package=${form_app}_${form_ver}_linux_${arch}.zip
+
+# Download packages
+#wget -N https://github.com/gruntwork-io/${grunt_app}/releases/download/v${grunt_ver}/${grunt_package}
+#wget -N https://releases.hashicorp.com/${form_app}/${form_ver}/${form_package}
 
 # Install packages
-chmod a+x $package
-sudo mkdir -p /opt/$app
-sudo mv $package /opt/$app/$app
+#sudo mkdir -p /opt/$grunt_app
+#chmod a+x $grunt_package
+#sudo mv $grunt_package /opt/$grunt_app/$grunt_app
+
+#sudo mkdir -p /opt/$grunt_app/$form_app
+#sudo unzip -o -u $form_package -d /opt/$grunt_app/$form_app
+#rm -f $form_package
 
 # Configure path
-sudo sh -c "echo 'export PATH=\$PATH:/opt/$app' > /etc/profile.d/${app}.sh"
+sudo sh -c "echo 'export PATH=\$PATH:/opt/$grunt_app\nexport TERRAGRUNT_TFPATH=/opt/$grunt_app/$form_app/$form_app' > /etc/profile.d/${grunt_app}.sh"
