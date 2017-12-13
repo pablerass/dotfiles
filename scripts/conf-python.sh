754261249110825631a337437994307e0d40fee0
Pablo Muñoz <pablerass@gmail.com>
2017-12-13 20:23:55 +0100

    Make python3 only installation and pipenv

--- scripts/conf-python.sh
+++ scripts/conf-python.sh
@@ -3,19 +3,16 @@
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 # Install packages
-sudo apt-get install python-pip python3-pip -y
-sudo apt-get install python-dev libffi-dev libssl-dev -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-dev libffi-dev libssl-dev -y
 
 # Update pip
 $DIR/update-pip.sh
 
 # Install python libs
-sudo -H pip3 install --upgrade \
+sudo -H pip3 install --upgrade --force \
 		jedi flake8 flake8-docstrings ropevim \
 		ipython[all] simplejson pyyaml \
-		requests[security] \
-		grip
-
-sudo -H pip install --upgrade \
 		virtualenv virtualenvwrapper \
-		jedi flake8 flake8-docstrings ropevim
+		requests[security] \
+		grip pipenv
