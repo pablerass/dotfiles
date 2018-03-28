9429d600894032afebc5739708d2f9088cb0d84f
Pablo Muñoz <pablerass@gmail.com>
2018-03-28 09:28:31 +0200

    Homogenize microsoft repo file names

--- scripts/install-mssql-tools.sh
+++ scripts/install-mssql-tools.sh
@@ -7,7 +7,7 @@ package=mssql-tools
 
 # Add repositories
 wget -q -O - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-wget -q -O - https://packages.microsoft.com/config/ubuntu/${os_version}/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+wget -q -O - https://packages.microsoft.com/config/ubuntu/${os_version}/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
 
 # Update repos
 sudo apt-get update -y
