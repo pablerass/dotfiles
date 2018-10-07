baf926846feb7bc9c10945360e7d681235d7022f
Pablo Muñoz <pablerass@gmail.com>
2018-10-07 21:11:42 +0200

    Add minikube script

---
+++ scripts/install-minikube.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+# Specify version
+app=minikube
+ver=0.30.0
+arch=amd64
+package=${app}-linux-${arch}
+
+# Download packages
+wget -N https://github.com/kubernetes/minikube/releases/download/v${ver}/${package}
+
+# Install packages
+sudo mkdir -p /opt/$app
+chmod a+x $package
+sudo mv $package /opt/$app/$app
+
+# Delete packages
+rm -f $package
+
+# Configure path
+sudo sh -c "echo 'export PATH=\$PATH:/opt/$app' > /etc/profile.d/${app}.sh"
