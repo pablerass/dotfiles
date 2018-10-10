99ba53f5c4beb2f164c996521be0e9e0d2062584
Pablo Muñoz <pablerass@gmail.com>
2018-10-10 12:12:12 +0200

    Tune gnome shell configuration

--- scripts/conf-gnome-shell.sh
+++ scripts/conf-gnome-shell.sh
@@ -4,6 +4,7 @@ BASE_DIR="$HOME/.local/share/gnome-shell/extensions"
 EXTENSIONS_URL="https://extensions.gnome.org/extension-data"
 EXTENSIONS="caffeine@patapon.info.v30.shell-extension_ov9BwbU.zip"
 
+# Install extensions
 for ext in ${EXTENSIONS[@]}; do
 	wget -N $EXTENSIONS_URL/$ext
 	ext_uuid=$(unzip -c $ext metadata.json | grep uuid | cut -d \" -f4)
@@ -12,5 +13,9 @@ for ext in ${EXTENSIONS[@]}; do
 	gnome-shell-extension-tool -e $ext_uuid || :
 	rm $ext
 done
+# Config workspaces
 gsettings set org.gnome.mutter workspaces-only-on-primary false
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+# Reload gnome-shell
 killall -3 gnome-shell
