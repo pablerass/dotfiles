db7d89cbde09257ce721f72aa517bfe0df2cd5ef
Pablo Muñoz <pablerass@gmail.com>
2018-10-16 12:55:59 +0200

    Add docker library for molecule driver

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 # Install packages
-sudo -H pip3 install --upgrade "ansible>=2.6.0<2.7.0" "molecule>=2.18.0<3.0.0"
+sudo -H pip3 install --upgrade "ansible>=2.6.0<2.7.0"
+sudo -H pip3 install molecule docker
