2a744f8b55a7cb2fc4610c6c7399ed698322d027
Pablo Muñoz <pablerass@gmail.com>
2019-09-10 18:54:06 +0200

    Correct visual studio code installation

--- scripts/install-visualstudiocode.sh
+++ scripts/install-visualstudiocode.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
-# Specify version
-app=code
-ver=1.32.1-1552006243_amd64
-arch=amd64
-package=${app}_${ver}_${arch}.deb
-cdn_hash=05f146c7a8f7f78e80261aa3b2a2e642586f9eb3
-
 # Download deb packages
 curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -19,6 +12,3 @@ sudo apt install code -y
 
 # Install extensions
 code --install-extension vscodevim.vim
-#code --install-extension ms-vscode.cpptools
-#code --install-extension lukehoban.Go
-#code --install-extension donjayamanne.python
