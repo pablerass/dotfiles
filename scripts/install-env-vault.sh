ece2b04176f9a021196b1bb5c2da2ea9a3d447b2
Pablo Muñoz <pablerass@gmail.com>
2019-10-16 20:04:23 +0200

    Install env vault script

---
+++ scripts/install-env-vault.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# Specify version
+app=env-vault
+ver=0.1.0
+arch=amd64
+package=${app}-linux-${arch}
+
+# Download packages
+wget -N https://github.com/pablerass/aws-vault/releases/download/v${ver}/${package}
+
+# Install packages
+sudo mkdir -p /opt/$app
+sudo mv $package /opt/$app/$app
+sudo chmod a+x /opt/$app/$app
+
+# Configure path
+sudo sh -c "echo 'export PATH=\$PATH:/opt/$app' > /etc/profile.d/${app}.sh"
