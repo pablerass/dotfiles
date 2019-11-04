2ff8a9a70d887bd679b54c6e2b8a6a19a93e98b0
Pablo Muñoz <pablerass@gmail.com>
2019-11-04 07:39:26 +0100

    Correct bfg installation script

--- scripts/install-bfg.sh
+++ scripts/install-bfg.sh
@@ -5,25 +5,24 @@ url="https://search.maven.org/remote_content?g=com.madgag&a=bfg&v=LATEST"
 app=bfg
 dir=/opt/$app
 jar=bfg.jar
-exe=bfg
 
 # Download BFG
 wget -O $jar $url
 
 # Create executable
-cat <<'EOF' > $exe
+cat <<'EOF' > $app
 #!/bin/bash
 
-java -jar /opt/bgf/bgf.jar $@
+java -jar /opt/bfg/bfg.jar $@
 EOF
 
 sudo chmod a+r $jar
-sudo chmod a+rx $exe
+sudo chmod a+rx $app
 
 # Move files
 sudo mkdir -p $dir
 
-sudo mv $exe $dir
+sudo mv $app $dir
 sudo mv $jar $dir
 
 sudo chown root:root -R $dir
