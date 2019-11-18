29725d03bd75311228f4c630452f32cb74824a0c
Pablo Muñoz <pablerass@gmail.com>
2019-11-18 08:13:22 +0100

    Remove verbosity and tabs in plugins script

--- setup/plugins.sh
+++ setup/plugins.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 ## Variables
 GITHUB_URL=https://github.com
@@ -10,54 +10,56 @@ VIM_PLUGINS_DIR=~/.vim/bundle
 
 ## Functions
 function update_repo {
-	LOCAL_DIR=$2
-	REPO=$1
-	if [ -d "$LOCAL_DIR" ]
-	then
-		git -C "$LOCAL_DIR" pull origin master
-	else
-		git clone $REPO "$LOCAL_DIR"
-	fi
+    LOCAL_DIR=$2
+    REPO=$1
+    echo "> Updating $LOCAL_DIR"
+    if [ -d "$LOCAL_DIR" ]; then
+        git -C "$LOCAL_DIR" pull origin master
+    else
+        git clone $REPO "$LOCAL_DIR"
+    fi
 }
 
 ## Links
 for plugin in $OH_MY_ZSH_OWN_CUSTOM_DIR/plugins/*; do
+    echo "> Updating $OH_MY_ZSH_PLUGINS_DIR/$(basename $plugin)"
     ln -sf $plugin $OH_MY_ZSH_PLUGINS_DIR/
 done
 
 for theme in $OH_MY_ZSH_OWN_CUSTOM_DIR/themes/*; do
+    echo "> Updating $OH_MY_ZSH_PLUGINS_DIR/themes/$(basename $theme)"
     ln -sf $theme $OH_MY_ZSH_CUSTOM_DIR/themes/
 done
 
 ## Repos
 # oh-my-zsh plugins
 update_repo $GITHUB_URL/jplitza/zsh-virsh-autocomplete.git \
-			$OH_MY_ZSH_PLUGINS_DIR/virsh
+            $OH_MY_ZSH_PLUGINS_DIR/virsh
 update_repo $GITHUB_URL/technosophos/glide-zsh.git \
-			$OH_MY_ZSH_PLUGINS_DIR/glide
+            $OH_MY_ZSH_PLUGINS_DIR/glide
 update_repo $GITHUB_URL/pablerass/zsh-terragrunt-autocomplete.git \
-			$OH_MY_ZSH_PLUGINS_DIR/terragrunt
+            $OH_MY_ZSH_PLUGINS_DIR/terragrunt
 update_repo $GITHUB_URL/matthieusb/zsh-sdkman \
-			$OH_MY_ZSH_PLUGINS_DIR/zsh-sdkman
+            $OH_MY_ZSH_PLUGINS_DIR/zsh-sdkman
 
 # Vim plugins
 update_repo $GITHUB_URL/scrooloose/nerdtree.git $VIM_PLUGINS_DIR/nerdtree
 update_repo $GITHUB_URL/vim-airline/vim-airline.git $VIM_PLUGINS_DIR/vim-airline
 update_repo $GITHUB_URL/vim-airline/vim-airline-themes.git \
-			$VIM_PLUGINS_DIR/vim-airline-themes
+            $VIM_PLUGINS_DIR/vim-airline-themes
 update_repo $GITHUB_URL/davidhalter/jedi-vim.git $VIM_PLUGINS_DIR/jedi-vim
 update_repo $GITHUB_URL/tpope/vim-fugitive.git $VIM_PLUGINS_DIR/vim-fugitive
 update_repo $GITHUB_URL/tpope/vim-surround $VIM_PLUGINS_DIR/vim-surround
 update_repo $GITHUB_URL/nvie/vim-flake8.git $VIM_PLUGINS_DIR/vim-flake8
 update_repo $GITHUB_URL/python-rope/ropevim.git $VIM_PLUGINS_DIR/ropevim
 update_repo $GITHUB_URL/JamshedVesuna/vim-markdown-preview.git \
-			$VIM_PLUGINS_DIR/vim-markdown-preview
+            $VIM_PLUGINS_DIR/vim-markdown-preview
 update_repo $GITHUB_URL/airblade/vim-gitgutter.git \
-			$VIM_PLUGINS_DIR/vim-gitgutter
+            $VIM_PLUGINS_DIR/vim-gitgutter
 update_repo $GITHUB_URL/vim-scripts/netrw.vim.git \
-			$VIM_PLUGINS_DIR/netwr
+            $VIM_PLUGINS_DIR/netwr
 update_repo $GITHUB_URL/Glench/Vim-Jinja2-Syntax.git \
-			$VIM_PLUGINS_DIR/vim-jinja2-syntax
+            $VIM_PLUGINS_DIR/vim-jinja2-syntax
 update_repo $GITHUB_URL/hashivim/vim-hashicorp-tools \
-			$VIM_PLUGINS_DIR/vim-hashicorp-tools
+            $VIM_PLUGINS_DIR/vim-hashicorp-tools
 
