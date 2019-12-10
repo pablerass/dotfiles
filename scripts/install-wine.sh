75b70a4772939e7fc0abe256af6042cfaf5310fc
Pablo Muñoz <pablerass@gmail.com>
2019-12-10 09:59:04 +0100

    Add install wine script

---
+++ scripts/install-wine.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+source /etc/lsb-release
+
+DISTRO=$DISTRIB_CODENAME
+
+# Add repositories
+sudo dpkg --add-architecture i386
+curl -s https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+sudo sh -c "echo deb https://dl.winehq.org/wine-builds/ubuntu/ $DISTRO main > /etc/apt/sources.list.d/wine.list"
+
+# Update repos
+sudo apt update -y
+
+# Install packages
+sudo apt install -y --install-recommends winehq-stable
