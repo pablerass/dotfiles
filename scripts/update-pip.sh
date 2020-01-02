e09335126e524cdea01fd52545248b6ad70b9429
Pablo Muñoz <pablerass@gmail.com>
2020-01-02 23:22:51 +0100

    Some tweaks on upgrade pip script

--- scripts/update-pip.sh
+++ scripts/update-pip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 # Update pip versions
-sudo -H pip2 install -U pip
-sudo -H pip3 install -U pip
+sudo -H /usr/local/bin/pip2 install --force-reinstall -U pip
+sudo -H /usr/local/bin/pip3 install --force-reinstall -U pip
 
