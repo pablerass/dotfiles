23bfaf6db29b54a811d262effa72a77d66c0d2e0
Pablo Muñoz <pablerass@gmail.com>
2020-02-03 12:07:17 +0100

    Try other way of updating pip without issues

--- scripts/update-pip.sh
+++ scripts/update-pip.sh
@@ -3,4 +3,4 @@
 # Update pip versions
 sudo -H /usr/local/bin/pip2 install --force-reinstall -U pip
 sudo -H /usr/local/bin/pip3 install --force-reinstall -U pip
-
+sudo -H /usr/local/bin/pip install --force-reinstall -U pip
