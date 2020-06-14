1c0dc55a019dd375270f9abeca6cde59327f955d
Pablo Muñoz <pablerass@gmail.com>
2020-06-14 17:21:09 +0200

    Update drawio version

--- scripts/install-drawio.sh
+++ scripts/install-drawio.sh
@@ -3,7 +3,7 @@
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 # Specify version
-ver=10.7.5
+ver=13.2.2
 package=draw.io-amd64-${ver}.deb
 
 # Download deb packages
@@ -14,7 +14,3 @@ sudo dpkg -i $package
 
 # Delete packages
 rm -f $package
-
-# Correct icon config
-sudo cp $DIR/img/draw-io.png /opt/draw.io
-sudo sed -i 's#^Icon=.*$#Icon=/opt/draw.io/draw-io.png#g' /usr/share/applications/draw.io.desktop
