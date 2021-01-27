47a65f1b9ca9c6a65d411a8b20e9fc1aa66b5961
Pablo Muñoz <pablerass@gmail.com>
2021-01-27 12:06:17 +0100

    Improve pyenv install script

--- scripts/install-pyenv.sh
+++ scripts/install-pyenv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
-sudo apt install curl git-core gcc make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libssl-dev
+sudo apt install curl git-core gcc make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libssl-dev \
+    llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+PYENV_DIR=$HOME/.pyenv
+if [ ! -e "$PYENV_DIR" ]; then
+    git clone https://github.com/pyenv/pyenv.git "$PYENV_DIR"
+else
+    git -C "$PYENV_DIR" pull origin master
+fi
