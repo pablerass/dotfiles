39f0a3603ea7c88677de1805aabe737c33fa960e
Pablo Muñoz <pablerass@gmail.com>
2023-12-30 09:02:04 +0100

    Add more zoom installation requirements

--- scripts/install-zoom.sh
+++ scripts/install-zoom.sh
@@ -7,7 +7,7 @@ package=zoom_amd64.deb
 wget -N https://zoom.us/client/latest/$package
 
 # Install dependencies
-sudo apt install -y libxcb-xtest0 libegl1-mesa libgl1-mesa-glx
+sudo apt install -y libxcb-xtest0 libxcb-cursor0 libegl1-mesa libgl1-mesa-glx
 
 # Install packages
 sudo dpkg -i $package
